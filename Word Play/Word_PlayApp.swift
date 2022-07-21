79de130237431e40e549841f08ba89891c65a35f
LinLin762 <lina@bhatia.org>
2022-07-21 10:29:26 -0500

    Initial Commit

---
+++ Word Play/Word_PlayApp.swift
@@ -0,0 +1,17 @@
+//
+//  Word_PlayApp.swift
+//  Word Play
+//
+//  Created by Lina Bhatia on 7/21/22.
+//
+
+import SwiftUI
+
+@main
+struct Word_PlayApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
